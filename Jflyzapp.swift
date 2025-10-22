04e7bdb3bc3baa5155c9195bd31e9ff8dba01443
James Oliveira <239462200+JflyZ-Ai@users.noreply.github.com>
2025-10-21 22:12:16 -0400

    Update Jflyzapp.swift

--- Jflyzapp.swift
+++ Jflyzapp.swift
@@ -1,1 +1,17 @@
+//
+//  JflyzApp.swift
+//  Jflyz AI
+//
+//  Created by James on JamesFlyzone
+//
 
+import SwiftUI
+
+@main
+struct JflyzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
